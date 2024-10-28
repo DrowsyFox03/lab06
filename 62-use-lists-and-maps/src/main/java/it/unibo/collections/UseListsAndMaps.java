91bb5af78f119a95a1a5547da5fe39c44462ad23
Volpini Francesco <francesco.volpini2@studio.unibo.it>
2024-10-28 12:58:31 +0100

    es completed

--- 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
+++ 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
@@ -1,13 +1,21 @@
 package it.unibo.collections;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Example class using {@link List} and {@link Map}.
  *
  */
 public final class UseListsAndMaps {
+
+    private static final int START = 1000;
+    private static final int STOP = 2000;
+    private static final int N_ELEMENTS = 10000;
+    private static final int READ_ITERATIONS = 1000;
 
     private UseListsAndMaps() {
     }
@@ -21,19 +29,30 @@ public final class UseListsAndMaps {
          * 1) Create a new ArrayList<Integer>, and populate it with the numbers
          * from 1000 (included) to 2000 (excluded).
          */
+        final ArrayList<Integer> arList = new ArrayList<>();
+        for (int i = START; i < STOP; i++){
+            arList.add(i);
+        }
         /*
          * 2) Create a new LinkedList<Integer> and, in a single line of code
          * without using any looping construct (for, while), populate it with
          * the same contents of the list of point 1.
          */
+        final List<Integer> lnkList = new LinkedList<>(arList);
         /*
          * 3) Using "set" and "get" and "size" methods, swap the first and last
          * element of the first list. You can not use any "magic number".
          * (Suggestion: use a temporary variable)
          */
+        final int temp = arList.getLast();
+        arList.set(arList.size() - 1, arList.getFirst());
+        arList.set(0, temp);
         /*
          * 4) Using a single for-each, print the contents of the arraylist.
          */
+        for (int i : arList) {
+            System.out.println(i);
+        }
         /*
          * 5) Measure the performance of inserting new elements in the head of
          * the collection: measure the time required to add 100.000 elements as
@@ -41,12 +60,42 @@ public final class UseListsAndMaps {
          * using the previous lists. In order to measure times, use as example
          * TestPerformance.java.
          */
+        long time = System.nanoTime();
+        for (int i = 0; i < N_ELEMENTS; i++){
+            arList.addFirst(0);
+        }
+        time = System.nanoTime() - time;
+        var millis = TimeUnit.NANOSECONDS.toMillis(time);
+        System.out.println("ArrayList time = " + time + "ns = " + millis + "ms");
+
+        time = System.nanoTime() - time;
+        for (int i = 0; i < N_ELEMENTS; i++){
+            lnkList.addFirst(0);
+        }
+        time = System.nanoTime() - time;
+        millis = TimeUnit.NANOSECONDS.toMillis(time);
+        System.out.println("LinkedList time = " + time + "ns = " + millis + "ms");
         /*
          * 6) Measure the performance of reading 1000 times an element whose
          * position is in the middle of the collection for both ArrayList and
          * LinkedList, using the collections of point 5. In order to measure
          * times, use as example TestPerformance.java.
          */
+        time = System.nanoTime();
+        for (int i = 0; i < READ_ITERATIONS; i++){
+            arList.get(arList.size() / 2);
+        }
+        time = System.nanoTime() - time;
+        millis = TimeUnit.NANOSECONDS.toMillis(time);
+        System.out.println("ArrayList time = " + time + "ns = " + millis + "ms");
+
+        time = System.nanoTime() - time;
+        for (int i = 0; i < READ_ITERATIONS; i++){
+            lnkList.get(lnkList.size() / 2);
+        }
+        time = System.nanoTime() - time;
+        millis = TimeUnit.NANOSECONDS.toMillis(time);
+        System.out.println("LinkedList time = " + time + "ns = " + millis + "ms");
         /*
          * 7) Build a new Map that associates to each continent's name its
          * population:
